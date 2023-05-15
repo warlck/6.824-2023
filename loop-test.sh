3e011bb8b7d507e21d881259becb9984ac765162
warlck <addick@me.com>
2023-05-15 10:27:47 +0800

    Added bash script helper to run the specific test in loop and record the verbose output of last failed test run

---
+++ loop-test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Runs the test provided by test name in loop until failure using Verbose mode. Records the verbose output of the failed test in 
+# output file. Need to be in the folder with the tests to run. 
+for i in {1..100}; do        
+    echo "Running iteration $i"
+    VERBOSE=1 go test -run $1 1> out.log 2>&1 || { echo "Test failed on attempt $i"; break; }
+done
